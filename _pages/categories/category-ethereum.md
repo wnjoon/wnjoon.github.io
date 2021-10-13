f7733188a20647958c6b25565b9b0b9daca2039d
wonjoon <39115630+wnjoon@users.noreply.github.com>
2021-10-13 13:40:04 +0900

    first commit

---
+++ _pages/categories/category-ethereum.md
@@ -0,0 +1,14 @@
+---
+title: "Ethereum"
+layout: archive
+permalink: categories/ethereum
+author_profile: false
+sidebar_main: true
+---
+
+<!-- 공백이 포함되어 있는 카테고리 이름의 경우 site.categories.['a b c'] 이런식으로! -->
+
+***
+
+{% assign posts = site.categories.Ethereum %}
+{% for post in posts %} {% include archive-single2.html type=page.entries_layout %} {% endfor %}
---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Infura로 이더리움 테스트용 노드 만들기",
+        "excerpt":"Infura 일반적으로 디앱(DApp)를 개발할 때에는 로컬에서 Ganashe 또는 Truffle develop을 사용하거나 geth를 이용한 private network를 구성한다. 물론 디앱에 국한되지 않더라도 대부분의 앱 또한 로컬에서 개발 -&gt; 테스트 -&gt; 실제 환경과 유사한 개발환경에서 테스트 -&gt; 배포와 같은 순서를 갖는다. 디앱은 여기서 조금 다른 부분이 테스트넷 환경에서 실제로 구성된 노드에 직접 테스트해봐야...","categories": ["Ethereum"],
+        "tags": ["Blockchain","Ethereum"],
+        "url": "http://localhost:4000/ethereum/infura/",
+        "teaser": null
+      },{
+        "title": "이더리움 표준(ERC)",
+        "excerpt":"ERC-20 Ethereum Request for Comment 20의 약자로, 이더리움 네트워크를 관리하는 단체인 EIPs(Ethereum Improvement Proposals)에서 관리하는 공식 프로토콜이다. 이더리움 블록체인 네트워크에서 정한 표준 토큰 스펙이다. 만들어진 배경 ERC-20은 이더리움 네트워크 상에서 유통되는 토큰의 호환성을 보장하기 위하여 제작되었다. 실제로 대다수의 암호화폐(토큰)가 이더리움 기반으로 생성되었고, 이러한 토큰은 자체 메인넷을 출시하면서 다른 이더리움 기반...","categories": ["Ethereum"],
+        "tags": ["Blockchain","Ethereum","Protocol"],
+        "url": "http://localhost:4000/ethereum/erc/",
+        "teaser": null
+      }]
