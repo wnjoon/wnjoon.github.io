fabb0c214040c8df56a5e057d2238ad58f33f577
wonjoon <39115630+wnjoon@users.noreply.github.com>
2021-10-13 16:00:00 +0900

    rebuild_avatar

---
+++ push.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+MESSAGE=$1
+
+echo "1. Add all"
+git add -A
+
+echo "2. commit"
+git commit -m $MESSAGE 
+
+echo "3. push"
+git push origin master
+
+echo "4. rebuild commit(dunno why)"
+git commit --allow-empty -m "rebuild"
+
+echo "5. repush"
+git push origin master
+
